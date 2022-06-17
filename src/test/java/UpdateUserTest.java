984d4a9eb91fefdc5c29a7b9843924a6d5d7f18a
darici001 <107683085+darici001@users.noreply.github.com>
2022-06-17 23:52:04 +0200

    updateUser

---
+++ src/test/java/UpdateUserTest.java
@@ -0,0 +1,21 @@
+import io.restassured.http.ContentType;
+
+import org.junit.jupiter.api.Test;
+import services.GoRestService;
+
+import static org.apache.http.HttpStatus.SC_OK;
+
+
+public class UpdateUserTest {
+    @Test
+    public void UserUpdate() {
+        GoRestService.updateUser()
+                .then()
+                .assertThat()
+                .statusCode(SC_OK)
+                .contentType(ContentType.JSON)
+                .extract()
+
+                .response().prettyPrint();
+    }
+}
