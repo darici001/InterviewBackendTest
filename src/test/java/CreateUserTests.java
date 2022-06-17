77a402f900a5385dbef8479ccab4be2187a47942
ali <comdarici@gmail.com>
2022-06-18 00:29:28 +0200

    faker

--- src/test/java/CreateUserTests.java
+++ src/test/java/CreateUserTests.java
@@ -1,3 +1,4 @@
+import com.github.javafaker.Faker;
 import models.CreateUserModel;
 import org.junit.jupiter.api.Test;
 import services.GoRestService;
@@ -7,15 +8,19 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 public class CreateUserTests {
-
+Faker faker = new Faker();
     @Test
     public void Users_CreateUsers_Success(){
 
-        final CreateUserModel createUserModel = new CreateUserModel("Gino Paloma", "Male", "qatest@test.com", "Active");
+        final CreateUserModel createUserModel = new CreateUserModel(faker.name().fullName(), "Male",
+                faker.internet().emailAddress(), "Active");
         GoRestService.createUser(createUserModel)
                 .then()
                 .statusCode(SC_CREATED)
                 .body("data.id", notNullValue())
                 .body("data.name", equalTo(createUserModel.getName()));
+
+        System.out.println(createUserModel.getName());
+        System.out.println(createUserModel.getEmail());
     }
 }
